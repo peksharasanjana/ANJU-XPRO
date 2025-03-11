d8cd7d014fbe576ee8e1093474c60bcd2c528ab5
peksharasanjana <ddmaduwa567@gmail.com>
2025-03-11 20:03:07 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~SMFHVJYC#4lInPn8uv-WyoBgvXf3XZgmIa_UCW1kTXVMe7m0rvQA",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~rZtFjDTT#3fFEQFqc3MfBn5TCAYFzV1P8vtdlIbWni3Fg0pxhDNo",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000",
 FOOTER: process.env.FOOTER || "> PODIYA-XR @PRO\n> Developed by Mr. Peksha\n> GitHub Repository: https://github.com/peksharasanjana/PODIYA-XR-\n💃 Premium Version | Advanced Features | Secure & Reliable💫\n© 2025 𝐏ⓞ𝐃𝐈𝐘𝐀-𝗫𝗥 @𝗣𝗥𝗢. All rights reserved.\n💃Our whatsapp channel: https://whatsapp.com/channel/0029Vaf6ZWsHrDZhvE0FZZ0S",
